5b6a8cc7f211a3b69c624a7ae04d177316967465
Jeroen Verseput <jeroen.verseput@topicus.nl>
2018-01-04 18:02:19 +0100

    seperated profile component

--- 2 - Basic Profile/src/app/app.module.ts
+++ 2 - Basic Profile/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
