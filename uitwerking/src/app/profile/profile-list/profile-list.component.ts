2c6683825cc0b08d4dd3a9948ce24a1f984ab2a4
Jeroen Versput <joen100@gmail.com>
2018-01-07 15:06:40 +0100

    toevoegen filter zonder gebruik van pipe

--- uitwerking/src/app/profile/profile-list/profile-list.component.ts
+++ uitwerking/src/app/profile/profile-list/profile-list.component.ts
@@ -5,14 +5,32 @@ import { ProfileService, IProfile } from '../profile.service';
     templateUrl: './profile-list.component.html'
 })
 export class ProfileListComponent implements OnInit {
-    listFilter: string;
     profiles: IProfile[];
+    filteredProfiles: IProfile[];
+
+    _listFilter: string;
+
+    get listFilter(): string {
+        return this._listFilter;
+    }
+
+    set listFilter(value: string) {
+        this._listFilter = value;
+        this.filteredProfiles = this.listFilter ? this.performFilter(this.listFilter) : this.profiles;
+    }
 
     constructor(private profileService: ProfileService) { }
 
     ngOnInit() {
         this.profileService.getProfiles().subscribe( profiles => {
             this.profiles = profiles;
+            this.filteredProfiles = this.profiles
         });
+    }
+
+    performFilter(filterBy: string): IProfile[] {
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.profiles.filter((profile: IProfile) => 
+            profile.name.toLocaleLowerCase().indexOf(filterBy) !== -1)
     }
 }
