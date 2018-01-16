1c3cf07290e966311d78758a8dcc083c31f3797f
Jeroen Verseput <jeroen.verseput@topicus.nl>
2018-01-16 17:32:02 +0100

    finished the basic guard

--- uitwerking/src/app/profile/profile-details/profile-details-guard.service.ts
+++ uitwerking/src/app/profile/profile-details/profile-details-guard.service.ts
@@ -1,9 +1,20 @@
-import { Injectable } from "@angular/core";
-import { CanActivate } from "@angular/router";
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, Router } from '@angular/router';
+import { isNumber } from 'util';
 
 @Injectable()
 export class ProfileDetailsGuardService implements CanActivate {
-    canActivate(): boolean {
+
+    constructor(private router: Router) { }
+
+    canActivate(route: ActivatedRouteSnapshot): boolean {
+        const id = Number(route.url[1].path);
+
+        if (!isNaN(id) && Number(id) > 0) {
+            return true;
+        }
+
+        this.router.navigate(['/profiles']);
         return false;
     }
 }
